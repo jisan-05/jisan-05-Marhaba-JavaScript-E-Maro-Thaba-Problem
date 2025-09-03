bc0dcae5013311b8f21a138387eac17ac6a9a7b3
jisan-05 <zishantanvir05@gmail.com>
2025-09-03 12:05:29 +0600

    complete chapter 2

---
+++ chapter-2-variable.js
@@ -0,0 +1,40 @@
+// practise variable Declare
+const age = 21;
+const cousen = 4;
+const student = 45;
+const marks = 90;
+const rooms = 20;
+const studyHour = 6;
+const total = 100;
+
+// practise - console.log()
+console.log(age)
+const booksToRead = 20;
+console.log(booksToRead)
+const wakeUpTime=7;
+console.log(wakeUpTime)
+
+// practise string console.log()
+const name = "Jisan";
+console.log(name)
+const address = "Gazipur";
+console.log(address)
+
+// practise boolean console.log()
+const isHappy = false;
+console.log(isHappy)
+const isMessageMeCrush = true;
+console.log(isMessageMeCrush)
+const celebrating = true;
+console.log(celebrating)
+
+
+// practise simple math 
+const earning = 100;
+const expense = 40;
+const ache = 100 - 40;
+console.log(ache)
+const hasPencel = 10;
+const usePencel = 2;
+const howMuchTimeUsePencel = hasPencel / usePencel;
+console.log(howMuchTimeUsePencel)
