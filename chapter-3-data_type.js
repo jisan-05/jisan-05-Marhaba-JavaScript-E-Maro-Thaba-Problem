e813fb559b8d938b0b5adbf73eb283cbdefc99b0
jisan-05 <zishantanvir05@gmail.com>
2025-09-03 12:33:42 +0600

    complete chapter 3

---
+++ chapter-3-data_type.js
@@ -0,0 +1,54 @@
+// practise
+const price = 555;
+console.log(typeof price)
+const learning = true;
+console.log(typeof learning)
+const city = "dhaka";
+console.log(typeof city)
+const temperature = 102.2;
+console.log(typeof temperature)
+
+// practise change variable value ;
+let age = 30;
+console.log(age)
+age = 20;
+console.log(age)
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney)
+
+// practise comment 
+// cricket score 
+let score = 150;
+console.log(15)
+
+
+// practise 
+const number1 = "20"
+const number2 = 10;
+const makeNum = parseInt(number1)
+console.log(makeNum + number2)
+
+const floatNumber = 20.32353;
+const convertIntNum = parseInt(floatNumber)
+console.log(convertIntNum)
+
+const name2 = "Jisan"
+console.log(parseInt(name2))
+
+const num1 = 3.456;
+const num2 = 2.789;
+const totalNum = num1 + num2
+console.log(totalNum.toFixed(2))
+
+const num3 = "56.78"
+const num4 = "50.71"
+const convertNumfornum3 = parseFloat(num3)
+const convertNumfornum4 = parseFloat(num4)
+console.log(parseFloat(convertNumfornum3+convertNumfornum4))
+
+
+const justNumber = "43.4543454334"
+const justNumberMakeFloat = parseFloat(justNumber)
+console.log(justNumberMakeFloat.toFixed(3))
