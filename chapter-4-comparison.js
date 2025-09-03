98e0ffb039f408b41386bf54652f94b2fe0a9790
jisan-05 <zishantanvir05@gmail.com>
2025-09-03 16:32:56 +0600

    complete chapter 4

---
+++ chapter-4-comparison.js
@@ -0,0 +1,32 @@
+// practice 
+const meEating = 7 ;
+const broEating = 5;
+if(meEating > broEating){
+    console.log("Ami Besi Khaisi")
+}else{
+    console.log("brother besi khaise")
+}
+
+// practise 
+const youngerBroAge = 10;
+const myAge = 15;
+const meIsBig = youngerBroAge < myAge;
+console.log(meIsBig)
+
+const passMark = 33;
+const myMark = 45;
+const isPass = passMark < myMark;
+console.log(isPass)
+
+
+// practise 
+console.log(50 == "50")
+console.log(50 === "50")
+
+console.log("JavaScript" == 'JavaScript')
+console.log("JavaScript" === 'JavaScript')
+
+
+console.log("test" == 'TEST')
+console.log("test" === 'TEST')
+
